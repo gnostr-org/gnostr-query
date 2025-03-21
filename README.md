5c0b131ff75e1a174b24873529d1d4e900822967
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2025-03-20 21:20:06 -0400

    gnostr-query:v0.0.1

---
+++ README.md
@@ -0,0 +1,18 @@
+## gnostr-query
+
+### construct nostr queries
+
+```sh
+Usage: gnostr-query [OPTIONS]
+
+Options:
+  -a, --authors <authors>        Comma-separated list of authors
+  -p, --mentions <mentions>      Comma-separated list of mentions
+  -e, --references <references>  Comma-separated list of references
+  -t, --hashtag <hashtag>        Comma-separated list of hashtags
+  -i, --ids <ids>                Comma-separated list of ids
+  -k, --kinds <kinds>            Comma-separated list of kinds (integers)
+  -g, --generic <tag> <value>    Generic tag query: #<tag>: value
+  -l, --limit <limit>            Limit the number of results
+  -h, --help                     Print help
+```
---
+++ src/main.rs
@@ -0,0 +1,153 @@
+use clap::{Arg, Command};
+use futures::{SinkExt, StreamExt};
+use serde_json::{json, to_string};
+use tokio::net::TcpStream;
+use tokio_tungstenite::{connect_async, tungstenite::Message, MaybeTlsStream, WebSocketStream};
+use url::Url;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let matches = Command::new("gnostr-query")
+        .about("Construct nostr queries and send them over a websocket")
+        .arg(
+            Arg::new("authors")
+                .short('a')
+                .long("authors")
+                .help("Comma-separated list of authors"),
+        )
+        .arg(
+            Arg::new("mentions")
+                .short('p')
+                .long("mentions")
+                .help("Comma-separated list of mentions"),
+        )
+        .arg(
+            Arg::new("references")
+                .short('e')
+                .long("references")
+                .help("Comma-separated list of references"),
+        )
+        .arg(
+            Arg::new("hashtag")
+                .short('t')
+                .long("hashtag")
+                .help("Comma-separated list of hashtags"),
+        )
+        .arg(
+            Arg::new("ids")
+                .short('i')
+                .long("ids")
+                .help("Comma-separated list of ids"),
+        )
+        .arg(
+            Arg::new("kinds")
+                .short('k')
+                .long("kinds")
+                .help("Comma-separated list of kinds (integers)"),
+        )
+        .arg(
+            Arg::new("generic")
+                .short('g')
+                .long("generic")
+                .value_names(["tag", "value"])
+                .number_of_values(2)
+                .help("Generic tag query: #<tag>: value"),
+        )
+        .arg(
+            Arg::new("limit")
+                .short('l')
+                .long("limit")
+                .value_parser(clap::value_parser!(i32))
+                .help("Limit the number of results"),
+        )
+        .arg(
+            Arg::new("relay")
+                .short('r')
+                .long("relay")
+                .required(false)
+                //.help("-r wss://relay.damus.io")
+                .default_value("wss://relay.damus.io"),
+        )
+        .get_matches();
+
+    let mut filt = serde_json::Map::new();
+
+    if let Some(authors) = matches.get_one::<String>("authors") {
+        filt.insert(
+            "authors".to_string(),
+            json!(authors.split(',').collect::<Vec<&str>>()),
+        );
+    }
+
+    if let Some(ids) = matches.get_one::<String>("ids") {
+        filt.insert(
+            "ids".to_string(),
+            json!(ids.split(',').collect::<Vec<&str>>()),
+        );
+    }
+
+    if let Some(limit) = matches.get_one::<i32>("limit") {
+        filt.insert("limit".to_string(), json!(limit));
+    }
+
+    if let Some(generic) = matches.get_many::<String>("generic") {
+        let generic_vec: Vec<&String> = generic.collect();
+        if generic_vec.len() == 2 {
+            let tag = format!("#{}", generic_vec[0]);
+            let val = generic_vec[1].split(',').collect::<Vec<&str>>();
+            filt.insert(tag, json!(val));
+        }
+    }
+
+    if let Some(hashtag) = matches.get_one::<String>("hashtag") {
+        filt.insert(
+            "#t".to_string(),
+            json!(hashtag.split(',').collect::<Vec<&str>>()),
+        );
+    }
+
+    if let Some(mentions) = matches.get_one::<String>("mentions") {
+        filt.insert(
+            "#p".to_string(),
+            json!(mentions.split(',').collect::<Vec<&str>>()),
+        );
+    }
+
+    if let Some(references) = matches.get_one::<String>("references") {
+        filt.insert(
+            "#e".to_string(),
+            json!(references.split(',').collect::<Vec<&str>>()),
+        );
+    }
+
+    if let Some(kinds) = matches.get_one::<String>("kinds") {
+        if let Ok(kind_ints) = kinds
+            .split(',')
+            .map(|s| s.parse::<i64>())
+            .collect::<Result<Vec<i64>, _>>()
+        {
+            filt.insert("kinds".to_string(), json!(kind_ints));
+        } else {
+            eprintln!("Error parsing kinds. Ensure they are integers.");
+            std::process::exit(1);
+        }
+    }
+
+    let q = json!(["REQ", "gnostr-query", filt]);
+    let query_string = to_string(&q)?;
+    let relay_url_str = matches.get_one::<String>("relay").unwrap();
+    let relay_url = Url::parse(relay_url_str)?;
+    let (ws_stream, _) = connect_async(relay_url).await?;
+    let (mut write, mut read) = ws_stream.split();
+
+    write.send(Message::Text(query_string)).await?;
+
+    while let Some(message) = read.next().await {
+        let data = message?;
+        if let Message::Text(text) = data {
+            println!("{}", text);
+        }
+    }
+
+    Ok(())
+}
