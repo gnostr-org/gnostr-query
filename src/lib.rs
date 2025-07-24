5652d5254b9d9fd135d78057ca429c3507508b1f
randymcmillan <randymcmillan@protonmail.com>
2025-07-24 10:03:44 -0400

    src/lib.rs:ConfigBuilder:initial impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,151 @@
+use futures::{SinkExt, StreamExt};
+use log::debug;
 use serde_json::{json, Map};
+use tokio_tungstenite::{connect_async, tungstenite::Message};
+use url::Url;
+
+#[derive(Debug)]
+pub struct Config {
+    host: String,
+    port: u16,
+    use_tls: bool,
+    retries: u8,
+    authors: String,
+    ids: String,
+    limit: i32,
+    generic: (String, String),
+    hashtag: String,
+    mentions: String,
+    references: String,
+    kinds: String,
+}
+
+#[derive(Debug, Default)]
+pub struct ConfigBuilder {
+    host: Option<String>,
+    port: Option<u16>,
+    use_tls: bool,
+    retries: u8,
+    authors: Option<String>,
+    ids: Option<String>,
+    limit: Option<i32>,
+    generic: Option<(String, String)>,
+    hashtag: Option<String>,
+    mentions: Option<String>,
+    references: Option<String>,
+    kinds: Option<String>,
+}
+impl ConfigBuilder {
+    pub fn new() -> Self {
+        ConfigBuilder {
+            host: None,
+            port: None,
+            use_tls: false,
+            retries: 0,
+            authors: None,
+            ids: None,
+            limit: None,
+            generic: None,
+            hashtag: None,
+            mentions: None,
+            references: None,
+            kinds: None,
+        }
+    }
+    pub fn host(mut self, host: &str) -> Self {
+        self.host = Some(host.to_string());
+        self
+    }
+    pub fn port(mut self, port: u16) -> Self {
+        self.port = Some(port);
+        self
+    }
+    pub fn use_tls(mut self, use_tls: bool) -> Self {
+        self.use_tls = use_tls;
+        self
+    }
+    pub fn retries(mut self, retries: u8) -> Self {
+        self.retries = retries;
+        self
+    }
+    pub fn authors(mut self, authors: &str) -> Self {
+        self.authors = Some(authors.to_string());
+        self
+    }
+    pub fn ids(mut self, ids: &str) -> Self {
+        self.ids = Some(ids.to_string());
+        self
+    }
+    pub fn limit(mut self, limit: i32) -> Self {
+        self.limit = Some(limit);
+        self
+    }
+    //pub fn generic(mut self, generic: &(&str, &str), tag: &str, val: &str) -> Self {
+    pub fn generic(mut self, tag: &str, val: &str) -> Self {
+        //self.generic = Some(("".to_string(), "".to_string()));
+        self.generic = Some((tag.to_string(), val.to_string()));
+        self
+    }
+    pub fn hashtag(mut self, hashtag: &str) -> Self {
+        self.hashtag = Some(hashtag.to_string());
+        self
+    }
+    pub fn mentions(mut self, mentions: &str) -> Self {
+        self.mentions = Some(mentions.to_string());
+        self
+    }
+    pub fn references(mut self, references: &str) -> Self {
+        self.references = Some(references.to_string());
+        self
+    }
+    pub fn kinds(mut self, kinds: &str) -> Self {
+        self.kinds = Some(kinds.to_string());
+        self
+    }
+    pub fn build(self) -> Result<Config, String> {
+        Ok(Config {
+            host: self.host.ok_or("Missing host")?,
+            port: self.port.ok_or("Missing port")?,
+            use_tls: self.use_tls,
+            retries: self.retries,
+            authors: self.authors.ok_or("")?,
+            ids: self.ids.ok_or("")?,
+            limit: self.limit.ok_or("")?,
+            generic: self.generic.ok_or("")?,
+            hashtag: self.hashtag.ok_or("")?,
+            mentions: self.mentions.ok_or("")?,
+            references: self.references.ok_or("")?,
+            kinds: self.kinds.ok_or("")?,
+        })
+    }
+}
+pub async fn send(
+    query_string: String,
+    relay_url: Url,
+    limit: Option<i32>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    //println!("\n{}\n", query_string);
+    //println!("\n{}\n", relay_url);
+    //println!("\n{}\n", limit.unwrap());
+    debug!("\n{}\n", query_string);
+    debug!("\n{}\n", relay_url);
+    debug!("\n{}\n", limit.unwrap());
+    let (ws_stream, _) = connect_async(relay_url).await?;
+    let (mut write, mut read) = ws_stream.split();
+    write.send(Message::Text(query_string)).await?;
+    let mut count: i32 = 0;
+    while let Some(message) = read.next().await {
+        let data = message?;
+        if count >= limit.unwrap() {
+            std::process::exit(0);
+        }
+        if let Message::Text(text) = data {
+            print!("{}", text);
+            count += 1;
+        }
+    }
+    Ok(())
+}
 
 pub fn build_gnostr_query(
     authors: Option<&str>,
